f7ec16cef870550d253ebe537413d6c5eae66766
Ariel Villarreal <arielskap@gmail.com>
2021-03-23 13:59:08 -0300

    Viendo que oinda

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,25 @@ import Layout from "@components/Layout"
 const Index: React.FC = () => {
 
 	const handleClick = () => {
-		const appId = 123175986433056
-		const redirectUrl = `playing-instagram.vercel.app`
-
-		fetch( `https://api.instagram.com/oauth/authorize
-				?client_id=${appId}
-				&redirect_uri=${redirectUrl}
-				&scope=user_profile,user_media
-				&response_type=code` )
+		// fetch( `https://www.instagram.com/ecstasy_ring/?__a=1` )
+		// 	.then( ( result ) => {
+		// 		return result.json()
+		// 	} ).catch( ( e ) => {
+		// 		console.log( e )
+		// 		return e
+		// 	} ).then( ( result ) => {
+		// 		console.log( result )
+		// 		return result
+		// 	} )
+		fetch( `https://www.instagram.com/ecstasy_ring/?__a=1`, {
+			method: `POST`,
+			body: JSON.stringify( {
+				client_id: 123175986433056,
+				client_secret: `873338430866df7c9899abd78280a8e9`,
+				grant_type: `authorization_code`,
+				code: `IGQVJWaS1GZAE5Iai1RU2dnNDVlTkFwVURFQ1hfUDRreGgyQU1zQWlJWWVhVzVPa25hakN0ODdyUndQUEl2cTlSUUc0dE1xM1E0cG50Qy1rVHRtZAjNoSHZACNm5WT2Fnd3pYOUlsa0RBamVBeDZAjMGhMcgZDZD`
+			} )
+		} )
 			.then( ( result ) => {
 				return result.json()
 			} ).catch( ( e ) => {
@@ -21,6 +32,24 @@ const Index: React.FC = () => {
 				console.log( result )
 				return result
 			} )
+		// IGQVJWaS1GZAE5Iai1RU2dnNDVlTkFwVURFQ1hfUDRreGgyQU1zQWlJWWVhVzVPa25hakN0ODdyUndQUEl2cTlSUUc0dE1xM1E0cG50Qy1rVHRtZAjNoSHZACNm5WT2Fnd3pYOUlsa0RBamVBeDZAjMGhMcgZDZD
+		// const appId = 123175986433056
+		// const redirectUrl = `playing-instagram.vercel.app`
+
+		// fetch( `https://api.instagram.com/oauth/authorize
+		// 		?client_id=${appId}
+		// 		&redirect_uri=${redirectUrl}
+		// 		&scope=user_profile,user_media
+		// 		&response_type=code` )
+		// 	.then( ( result ) => {
+		// 		return result.json()
+		// 	} ).catch( ( e ) => {
+		// 		console.log( e )
+		// 		return e
+		// 	} ).then( ( result ) => {
+		// 		console.log( result )
+		// 		return result
+		// 	} )
 		// const instagramUrl = `https://www.instagram.com/p/fA9uwTtkSN/`
 		// const accessToken = `6bf92fe3c5396340f44799c9b73d7970`
 
@@ -36,11 +65,28 @@ const Index: React.FC = () => {
 		// 	} )
 	}
 
+	const otro = () => {
+		const instagramUrl = `https://www.instagram.com/p/fA9uwTtkSN/`
+		const accessToken = `IGQVJWaS1GZAE5Iai1RU2dnNDVlTkFwVURFQ1hfUDRreGgyQU1zQWlJWWVhVzVPa25hakN0ODdyUndQUEl2cTlSUUc0dE1xM1E0cG50Qy1rVHRtZAjNoSHZACNm5WT2Fnd3pYOUlsa0RBamVBeDZAjMGhMcgZDZD`
+
+		fetch( `https://graph.facebook.com/v10.0/instagram_oembed?url=${instagramUrl}&access_token=${accessToken}` )
+			.then( ( result ) => {
+				return result.json()
+			} ).catch( ( e ) => {
+				console.log( e )
+				return e
+			} ).then( ( result ) => {
+				console.log( result )
+				return result
+			} )
+	}
+
 	return (
 		<Layout>
-			<div className='flex items-center justify-center w-screen h-screen'>
+			<div className='flex flex-col items-center justify-center w-screen h-screen'>
 				<HolaMundo />
 				<button type='button' onClick={handleClick}>Traer ultima publicación</button>
+				<button type='button' onClick={otro}>otro</button>
 			</div>
 		</Layout>
 	)
